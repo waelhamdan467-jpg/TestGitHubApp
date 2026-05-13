d2dfc5c89b904f8f736fa2ce6cace0c9bff63259
Wael Hamdan <waelhamdan467@gmail.com>
2026-05-13 14:15:08 +0300

    Test one

--- app/src/main/java/com/example/testtogithubproject/MainActivity.java
+++ app/src/main/java/com/example/testtogithubproject/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // i need to finsh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
